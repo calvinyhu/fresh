f4c6fbce28edf97486fb20a115cc300d273bd9b3
Calvin Hu <ycalvinhu@gmail.com>
2020-05-23 18:24:20 -0700

    init

---
+++ ubuntu.sh
@@ -0,0 +1,10 @@
+# Update system packages
+sudo apt update
+sudo apt upgrade
+sudo apt autoremove
+
+# Install commonly used developer tools
+sudo apt install build-essential
+
+# Install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
